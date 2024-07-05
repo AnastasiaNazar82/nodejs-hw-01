96335d8c2a9072833630c02becfa7fe776d9ed5e
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-07-05 23:54:21 +0300

    add

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { getAllContacts } from './getAllContacts.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    try {
+        
+    }
+};
 
 generateContacts(5);
